c9768a737c5327609ebd161ddf6756a563a4ed4a
FrancoTelliz <francotellizft@gmail.com>
2022-07-30 03:34:42 -0300

    Agrego primer query hardcodeado

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoryService {
-    async getMany(){}
+    async getMany(){
+        return [{
+            id: '2e1321',
+            createAt: new Date(),
+            updateAt: new Date(),
+            name : 'Ejemplo Query'
+        }]
+    }
     async get(){}
     async create(){}
     async update(){}
